94ff596b91ed2723c1bcbcf08aa01ed96e5f6f70
GuRuGu <peter.k.nets@gmail.com>
2020-02-13 07:43:53 +0200

    Use cache for upvoting

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -40,7 +40,12 @@ class LinkList extends Component {
           return (
             <div>
               {linksToRender.map((link, index) => (
-                <Link key={link.id} link={link} index={index} />
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           );
@@ -48,6 +53,15 @@ class LinkList extends Component {
       </Query>
     );
   }
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
 }
 
 export default LinkList;
